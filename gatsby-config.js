1b17eb038af140f58d90869cc522d4a3be0e609b
Juan von Doom <phazhion@gmail.com>
2019-09-07 10:07:26 -0600

    Updated Site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bank Wiki`,
+    description: `Bank Wiki landing page`,
+    author: `@jvondoom`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
